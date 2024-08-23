1d90523f0220e4ecb033db9381a063ecd7e94d56
baqsia <baqsiaa@gmail.com>
2024-08-24 03:26:43 +0400

    just refactor

--- Joker/Lap.cs
+++ Joker/Lap.cs
@@ -5,22 +5,11 @@ namespace Joker;
 public record Lap(int Index)
 {
     public Player Run(Player starter, Players players, CardColor? trump)
-    { 
+    {
         var firstCard = starter.FindRandomCard();
-        LapCards lapCards = [new PlayerCard(firstCard, 0, starter.Index)];
-         
-        var waitingPlayerCount = players.Where(a => !a.Starter)
-            .Select(a => a.Index)
-            .ToArray();
+        LapCards lapCards = LapCards.Unfold(starter, players, firstCard);
 
-        foreach (var index in waitingPlayerCount)
-        {
-            var player = players.ElementAt(index);
-            var lapCard = player.TryFindSameCardByColor(firstCard.Color, index);
-            lapCards.Add(lapCard);
-        } 
-
-        if(trump.HasValue)
+        if (trump.HasValue)
         {
             return players.ElementAt(
                 lapCards
@@ -51,5 +40,23 @@ public class LapCards : List<PlayerCard>
     {
         var orderedCards = this.Where(card => card.Card.Color == trump).OrderBy(card => card.CardIndex);
         return orderedCards?.MaxBy(a => a.Card?.Weight ?? 0)!;
+    }
+
+    public static LapCards Unfold(Player starter, Players players, Card firstCard)
+    {
+        LapCards lapCards = [new PlayerCard(firstCard, 0, starter.Index)];
+
+        var waitingPlayerCount = players.Where(a => !a.Starter)
+            .Select(a => a.Index)
+            .ToArray();
+
+        foreach (var index in waitingPlayerCount)
+        {
+            var player = players.ElementAt(index);
+            var lapCard = player.TryFindSameCardByColor(firstCard.Color, index);
+            lapCards.Add(lapCard);
+        }
+
+        return lapCards;
     }
 }
